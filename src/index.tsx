920268d5207f20306d56a3da92f427472edeeb43
LeandroMenoci <menoci.leandro@gmail.com>
2022-04-28 14:50:19 -0300

    React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
+import Inicio from 'pages/Inicio';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppRouter from 'routes';
 import './index.css';
 import Cardapio from './pages/Cardapio';
 
-
+const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
-
+    <AppRouter />
   </React.StrictMode>
 );
