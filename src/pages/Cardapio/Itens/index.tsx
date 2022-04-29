45027dab204afbfb037770a18eed10b36ef8dd0e
LeandroMenoci <menoci.leandro@gmail.com>
2022-04-28 23:33:21 -0300

    Rota Pratos / useLocation / Passar estados entre rotas

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,6 +2,7 @@ import cardapio from 'data/cardapio.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useEffect, useState } from 'react';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
   busca: string
@@ -23,16 +24,16 @@ export default function Itens(props: Props) {
     return true;
   }
 
-  function ordenar(novaLista: typeof cardapio) {
+  function ordenar(novaLista: Cardapio) {
     switch (ordenador) {
-    case 'porção':
-      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-    case 'qtd_pessoas':
-      return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-    case 'preco':
-      return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-    default:
-      return novaLista;
+      case 'porção':
+        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas':
+        return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+      case 'preco':
+        return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+      default:
+        return novaLista;
     }
 
   }
